335677940db48d2bf42f0627b0a02d157e25cdf9
Jiaxi Yuan <lynxyjx7@gmail.com>
2025-09-14 13:09:14 +0200

    Added answers to Exercise 4

--- Week-1/Exercises/Exercise-4.py
+++ Week-1/Exercises/Exercise-4.py
@@ -19,7 +19,7 @@ dct = {'a': 3, 'b': 7, 'c': -2, 'd': 10, 'e': 5}
 print("Exercise 4.1")
 
 pass
-
+print(sum(dct.values()))
 print("---")
 
 """
@@ -31,6 +31,9 @@ Print the key that has the largest value in dct.
 """
 
 print("Exercise 4.2")
+
+max_value_key = max(dct, key = dct.get)
+print(max_value_key)
 
 pass
 
@@ -47,6 +50,8 @@ Create a new dictionary with the squares of all the values in dct.
 print("Exercise 4.3")
 
 pass
+squared_dct = {key: value**2 for key, value in dct.items()}
+print(squared_dct)
 
 print("---")
 
@@ -61,7 +66,9 @@ Print only the keys in dct whose values are even numbers.
 print("Exercise 4.4")
 
 pass
-
+for key, value in dct.items():
+  if value % 2 == 0:
+    print(key)
 print("---")
 
 """
@@ -73,7 +80,8 @@ Create a new dictionary that swaps the keys and values in dct.
 """
 
 print("Exercise 4.5")
-
+dct_swap = {value: key for key, value in dct.items()}
+print(dct_swap)
 pass
 
 print("---")
@@ -90,7 +98,10 @@ and print the resulting dictionary.
 s = 'ccctcctttttcc'
 
 print("Exercise 4.6")
-
+counter_s = {}
+for letter in s:
+  counter_s[letter] = counter_s.get(letter, 0) + 1
+print(counter_s)
 pass
 
 print("---")
@@ -110,6 +121,8 @@ responses = 'jjjpjjpppppjj'
 
 print("Exercise 4.7")
 
+fullname = [responses_mapping[x] for x in responses]
+print(fullname)
 pass
 
 print("---")
@@ -126,7 +139,10 @@ Merge the following two dictionaries into one:
 print("Exercise 4.8")
 
 pass
-
+d1 = {'a': 1, 'b': 2}
+d2 = {'c': 3, 'd': 4}
+merged_d = {**d1, **d2}
+print(merged_d)
 print("---")
 
 """
@@ -141,7 +157,9 @@ create a new one whose keys are sorted alphabetically.
 print("Exercise 4.9")
 
 pass
-
+d1_animals = {'zebra': 10, 'dolphin': 25, 'alligator': 3, 'monkey': 5, 'pig': 9}
+d_sorted = {animals: d1_animals[animals] for animals in sorted(d1_animals)}
+print(d_sorted)
 print("---")
 
 """
@@ -156,5 +174,6 @@ create a new one whose values appear in increasing order.
 print("Exercise 4.10")
 
 pass
-
+d_sorted_num = {animals: number for animals, number in sorted(d1_animals.items(), key = lambda item: item[1])}
+print(d_sorted_num)
 print("---")
