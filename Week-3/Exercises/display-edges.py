63e9746785475ff62a12de9d47d20eeeed286b8d
LynxYJX <123210528+LynxYJX@users.noreply.github.com>
2025-09-28 16:06:24 +0200

    Update display-edges.py

--- Week-3/Exercises/display-edges.py
+++ Week-3/Exercises/display-edges.py
@@ -1,43 +1,64 @@
-# 1: Global Setting
 # Import the main modules of expyriment
+import expyriment
 from expyriment import design, control, stimuli
+from expyriment.misc import constants
+from expyriment.misc.constants import C_GREY
 
-# Create an object of class Experiment: This stores the global settings of your experiment & handles the data file, screen, and input devices
-exp = design.Experiment(name="Display Edges")
+# 1: Global Setting
+# Create an object of class Experiment
+exp = design.Experiment(name="Squares at Edges")
+control.set_develop_mode(on=True)  # Toggle to check scaling independence
 control.initialize(exp)
-# 2: Stimuli and Design
 
-# basic design of heights, widths 
-width, height = exp.screen.size # acquire the height and width of screen
-square_side_length = int(0.05 * width)  # 5% of the screen width
-half_side = square_side_length / 2
+# 2: Design & Set Ups
+# Get the screen dimensions
+screen_width, screen_height = exp.screen.size
 
+# Find the length of the square: 5% of screen width
+square_len = 0.05 * screen_width
+square_size = (square_len, square_len)
 
+# Find centers of these squares
+half_w = screen_width / 2 - square_len / 2
+half_h = screen_height / 2 - square_len / 2
 
-# find the center of squares
-position_x = width/2 - half_side 
-position_y = height/2 - half_side
+# Compute squares' centers
+positions = []
+for x in (-screen_width, screen_width):
+    for y in (-screen_height, screen_height):
+        # Ddetermine the direction by the sign of x and y.
+        sign_x = 1 if x > 0 else -1
+        sign_y = 1 if y > 0 else -1
+        
+        adjusted_x = (x / 2) - (sign_x * square_len / 2)
+        adjusted_y = (y / 2) - (sign_y * square_len / 2)
+        
+        positions.append((adjusted_x, adjusted_y))
 
-# find the position of squares
-positions = [
-    (-position_x, position_y),
-    (position_x, position_y),
-    (-position_x, -position_y),
-    (position_x, position_y)
-]
-# generate the squares
-for pos in positions:
-    rectangle = stimuli.Rectangle(size=(square_side_length,
-                                    square_side_length), 
-                                    position = pos,
-                                    line_width = 1,
-                                    line_color=(255, 0, 0))
+# Create stimuli
+squares = [expyriment.stimuli.Rectangle(
+    size=square_size,
+    position=pos,
+    colour=constants.C_RED,
+    line_width=1) for pos in positions]
+
+# Preload all stimuli to prevent loading
+for square in squares:
+    square.preload()
+
+# 3: Execution of stimuli & Start the experiment
+expyriment.control.start()
+
+# Create the canvas
+canvas = expyriment.stimuli.Canvas(size=exp.screen.size)
+
+for square in squares:
     square.plot(canvas)
-
+# Update the screen
 canvas.present()
 
-# Wait for any key to be pressed
+# Wait till key press
 exp.keyboard.wait()
 
-# 3: End 
+# End the experiment
 expyriment.control.end()
