aee59907bb8a1cd38390d195e277c8cdc0e2d5c4
LynxYJX <123210528+LynxYJX@users.noreply.github.com>
2025-10-05 14:44:49 +0200

    Update square_fixation.py

--- Week-4/Exercises/square_fixation.py
+++ Week-4/Exercises/square_fixation.py
@@ -5,15 +5,20 @@ exp = design.Experiment(name="Square")
 control.set_develop_mode()
 control.initialize(exp)
 
+# 2: Stimuli
 fixation = stimuli.FixCross()
 square = stimuli.Rectangle(size=(100, 100), line_width=5)
 
+# 3: Experiment Execution
 control.start(subject_id=1)
 
+# Present the fixation cross by itself
 fixation.present(clear=True, update=True)
 exp.clock.wait(500)
 
-fixation.present(clear=True, update = False)
+# Clear the back buffer and draw the fixation cross onto it
+fixation.present(clear=True, update = False) # Added line 
+# Draw the square onto the same back buffer
 square.present(clear=False, update=True)
 exp.keyboard.wait()
 
